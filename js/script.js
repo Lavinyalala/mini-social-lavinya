46d5bb26fd73264282feb00b085aaa816bb2876b
Lavinyalala <lavinya.9471117@aluno.mg.gov.br>
2026-04-08 12:29:49 +0000

    123132

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 let likeCount = 0;
-let curtido = false; //flag booleana
+let curtido = false; // flag booleana
+
+let deslikeCount = 0;
+let descurtido = false; // flag boolena
 
 function curtir() {
 
-if(curtido == false){
+ if (curtido == false){
    likeCount++;
-   curtido = true;
-  document.getElementById("likeCount").innerText = likeCount;
-} else{
-   likeCount--;
-   curtido = false;
+   curtido= true;
+   document.getElementById("likeCount").innerText = likeCount;
+}else{
+  likeCount--;
+  curtido = false;
   document.getElementById("likeCount").innerText = likeCount;
 }
+
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
+
+function descurtir() {
+ if (descurtido == false){
+     deslikeCount++;
+     descurtido = true;
+    document.getElementById("deslikeCount").innerText = deslikeCount;
+  }else{
+    deslikeCount--;
+    descurtido = false;
+    document.getElementById("deslikeCount").innerText = deslikeCount;
+  }
+
+}
+
+document.getElementById("likeBtn").addEventListener("click", curtir);
+document.getElementById("deslikeBtn").addEventListener("click", descurtir);
+
